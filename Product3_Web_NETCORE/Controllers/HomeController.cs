410fe24e595723f67321a8895187dc9434eda554
Carlos Manuel Ojeda Hernández <Carlos_moh@outlook.com>
2025-03-11 10:39:06 -0600

    Subieron estructura del proyecto

---
+++ Product3_Web_NETCORE/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Product3_Web_NETCORE.Models;
+using System.Diagnostics;
+
+namespace Product3_Web_NETCORE.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Product3_Web_NETCORE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Product3_Web_NETCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
