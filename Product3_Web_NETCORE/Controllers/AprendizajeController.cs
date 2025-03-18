9fc2a75b26838dab2ba158bd87bab2cdadc5ade9
Carlos Manuel Ojeda Hernández <Carlos_moh@outlook.com>
2025-03-18 16:54:52 -0600

    Actualizacion de gitignore

---
+++ Product3_Web_NETCORE/Controllers/AprendizajeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Product3_Web_NETCORE.Controllers
+{
+    public class AprendizajeController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Arrays()
+        {
+            return View();
+        }
+    }
+}
